145bff5bd573da881fd381398de7aae8272625c8
jalalatdin7413 <ramanovjalalatdin@gmail.com>
2024-12-06 18:48:36 +0500

    chore: updated UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,18 +7,4 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     
-    public function index() 
-    {
-        return 'Bul userler kestesi';
-    }
-
-    public function show($user) 
-    {
-        return 'Saylangan user: ' . $user;
-    }
-
-    public function create() 
-    {
-        return view('users.create');
-    }
 }
